eaab1b72ead503e3ed9c295eb3890dfcc10837e7
Mateus Varelo <mateus.varelo@ifood.com.br>
2023-09-20 21:15:24 -0300

    Infra airbyte e bucket

--- projeto/equipe_c/sources/worksheet/main.tf
+++ projeto/equipe_c/sources/worksheet/main.tf
@@ -16,3 +16,42 @@ provider "aws" {
     }
   }
 }
+
+# Resource Datalake
+resource "aws_s3_bucket" "datalake" {
+  bucket = "prd-datalake-team-c"
+
+  tags = {
+    Name        = "Team C"
+    Environment = "Dev"
+  }
+}
+
+# Bucket Block
+resource "aws_s3_bucket_public_access_block" "block_datalake" {
+  bucket = aws_s3_bucket.datalake.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Usuário airbyte acessos
+resource "aws_iam_user" "airbyte-stream" {
+  name = "user_airbyte"
+
+  tags = {
+    tag-key = "user_airbyte"
+  }
+}
+
+resource "aws_iam_access_key" "ak_airbyte_user" {
+  user = aws_iam_user.airbyte-stream.name
+}
+
+resource "aws_iam_user_policy" "airbyte_policy" {
+  name   = "airbyte_policy"
+  user   = aws_iam_user.airbyte-stream.name
+  policy = file("${path.module}/policy-airbyte-user.json")
+}
