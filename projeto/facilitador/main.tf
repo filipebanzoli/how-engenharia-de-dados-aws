00d3cf41e649b0bbdb4a7dc65106b636654207f7
Filipe Banzoli <filipebanzoli1@gmail.com>
2023-09-25 14:44:13 -0300

    creating facilitador module

---
+++ projeto/facilitador/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Owner = "facilitador"
+    }
+  }
+}
+
+module "transactional_database" {
+  source = "../geral/sources/transactional_database"
+}
+
+module "worksheet" {
+  source = "./sources/worksheet"
+}
+
