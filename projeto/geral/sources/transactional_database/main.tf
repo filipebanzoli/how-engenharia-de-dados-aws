5726db0a263f4214a599de11f69200875b30de68
Filipe Banzoli <filipebanzoli1@gmail.com>
2023-10-08 21:40:53 -0300

    including dms cdc project

--- projeto/geral/sources/transactional_database/main.tf
+++ projeto/geral/sources/transactional_database/main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "dms_sg" {
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "dms_egress" {
+resource "aws_vpc_security_group_egress_rule" "dms_egress_to_db" {
   security_group_id            = aws_security_group.dms_sg.id
   description                  = "Access to Transactional Postgres Database"
   ip_protocol                  = "-1"
@@ -91,13 +91,13 @@ resource "aws_vpc_security_group_egress_rule" "dms_egress" {
 resource "random_password" "postgres_transactional_root_password" {
   length           = 16
   special          = true
-  override_special = "!$-+<>:"
+  override_special = "!$-<>:"
 }
 
 resource "random_password" "postgres_transactional_fake_data_password" {
   length           = 16
   special          = true
-  override_special = "!$-+<>:"
+  override_special = "!$-<>:"
 }
 
 resource "aws_db_parameter_group" "transactional" {
