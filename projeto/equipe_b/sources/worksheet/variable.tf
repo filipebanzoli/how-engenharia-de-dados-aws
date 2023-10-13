75dd15dbb7a398c8063f703432aad3ebfadd6efa
Lorran <lorranpimenta@gmail.com>
2023-10-13 20:13:44 -0300

    Projeto antigo da equipe D

---
+++ projeto/equipe_b/sources/worksheet/variable.tf
@@ -0,0 +1,30 @@
+variable "aws_access_key_id" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_access_secret_key" {
+  type        = string
+  description = "AWS Access Secret Key"
+}
+
+variable "aws_session_token" {
+  type        = string
+  description = "AWS Session Token"
+}
+
+
+# variable "airbyte_api_keys" {
+#   type        = string
+#   description = "Airbyte Api Key"
+# }
+
+# variable "airbyte_username" {
+#   type        = string
+#   description = "Airbyte username"
+# }
+
+# variable "airbyte_password" {
+#   type        = string
+#   description = "Airbyte password"
+# }
