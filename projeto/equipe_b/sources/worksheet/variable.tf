453e0ea407eb1f5823e9994e09cdeb1eacaeb90f
Lorran <lorranpimenta@gmail.com>
2023-10-13 21:55:03 -0300

    Ajuste de projetos equipe D para equipe B

--- projeto/equipe_b/sources/worksheet/variable.tf
+++ projeto/equipe_b/sources/worksheet/variable.tf
@@ -12,17 +12,3 @@ variable "aws_session_token" {
   type        = string
   description = "AWS Session Token"
 }
-# variable "airbyte_api_keys" {
-#   type        = string
-#   description = "Airbyte Api Key"
-# }
-
-# variable "airbyte_username" {
-#   type        = string
-#   description = "Airbyte username"
-# }
-
-# variable "airbyte_password" {
-#   type        = string
-#   description = "Airbyte password"
-# }
