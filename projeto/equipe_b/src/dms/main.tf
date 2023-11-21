a87c53549cbfda21128618850d0a80c5fcb5e10d
Henrique Borges <henriquemauler@gmail.com>
2023-11-21 20:30:18 +0000

    free double check

--- projeto/equipe_b/src/dms/main.tf
+++ projeto/equipe_b/src/dms/main.tf
@@ -301,10 +301,10 @@ resource "aws_s3_bucket" "my_tf_access" {
 
 resource "aws_s3_bucket_public_access_block" "my_tf_access_access_block" {
   bucket = aws_s3_bucket.my_tf_access.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 
