8ae0f96410e8181128ffca3081bb5def4bb5a820
Fernando <f.fabiogama88@gmail.com>
2023-12-09 15:23:53 -0300

    residual

--- projeto/equipe_d/sources/worksheet/main.tf
+++ projeto/equipe_d/sources/worksheet/main.tf
@@ -9,10 +9,15 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
   default_tags {
     tags = {
       Owner = "equipe_d"
     }
   }
+}
+
+# Criação do Bucket S3
+resource "aws_s3_bucket" "example" { 
+  bucket = "my-bucket-equipe-d" 
 }
